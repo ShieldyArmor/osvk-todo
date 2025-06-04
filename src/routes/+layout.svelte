ce351291f6ec005ce12939c511652dfd376d9aed
ShieldyArmor <53658979+ShieldyArmor@users.noreply.github.com>
2025-06-04 07:47:05 +0200

    front-page setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,65 @@
+<script>
+
+    let searchTerm = ""
+                    
+</script>
+
+
+<header class="headBar">   
+    <nav class="navBar">
+    <a href="/" id="title">To-Do</a>
+    <a href="/guide" class="link">Guide</a>
+    <a href="/" class="link">Log in</a>
+    </nav>
+</header>
+
+<slot />
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+
+    .headBar {
+        position: sticky;
+        top: 0;
+        align-items: center;
+        display: flex;
+        width: 100%;
+        
+        height: fit-content;
+        min-height: 50px;
+        height: 9vh;
+        max-height: 75px;
+        border-bottom: 5px;
+        border-color: black;
+        border-style: solid;
+        background-color: #f8f9fa;
+    }
+    
+    .navBar {
+        padding: 0 20px;
+        width: 100%;
+    }
+
+    .navBar a {
+        display: block;
+        text-align: center;
+        margin: 0 8px;
+    }
+
+    #title {
+        float: left;
+        text-decoration: none;
+        color: Black;
+    }
+
+    .link {
+        float: right;
+        text-decoration: none;
+        color: black;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+      let todoLists = ["Groceries", "Homework", "Project Ideas"];
+
+  // Function to add a new list (optional)
+  function addList(title) {
+    if (title) {
+      todoLists = [...todoLists, title];
+    }
+  }
+
+</script>
+
+<svelte:head>
+  <title>OSVK To-Do</title>
+</svelte:head>
+
+<main>
+    <div>
+        <div class="listHeader">
+            <h1>Your To-Do lists:</h1>
+        </div>
+        <div class="listList">
+            {#each todoLists as title}
+              <div class="listItem">{title}</div>
+            {/each}
+        </div>
+    </div>
+</main>
+
+<style>
+    main {
+        height: 1000px;
+    }
+
+    .listHeader {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100px;
+        background-color: #f8f9fa;
+        border-bottom: 2px solid #ccc;
+    }
+
+    .listList {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+    }
+</style>
